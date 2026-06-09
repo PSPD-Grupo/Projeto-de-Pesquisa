5fc8f5f59c6b844e764072325feadc3265514bba
gabriel freitas <gabriel.fbalbino@gmail.com>
2026-06-08 22:23:26 -0300

    Docker compose do stub

--- docs/microsservicos/ServidorA/internal/persistence/desabafo.go
+++ docs/microsservicos/ServidorA/internal/persistence/desabafo.go
@@ -19,16 +19,12 @@ func NewDesabafoDb(texto string) *desabafoDb {
 }
 
 func (d *desabafoDb) Insert() error {
-<<<<<<< HEAD
-	result, err := Db.Exec(`INSERT INTO desabafo (texto, created_at) VALUES (?, ?);`, d.Texto, d.Created_at)
-=======
 	result, err := Db.Exec(
 		`INSERT INTO desabafo (id, texto, created_at)
-		VALUES ((SELECT COALESCE(MAX(id), 0) + 1 FROM desabafo), ?, ?)`,
+		VALUES ((SELECT COALESCE(MAX(id), 0) + 1 FROM desabafo), ?, ?);`,
 		d.Texto,
 		d.Created_at,
 	)
->>>>>>> 4551b46 (Server stub)
 	if err != nil {
 		return err
 	}
@@ -43,11 +39,7 @@ func (d *desabafoDb) Insert() error {
 }
 
 func (db *DataBase) GetNdesabafos(n int) ([]desabafoDb, error) {
-<<<<<<< HEAD
-	rows, err := db.db.Query(" SELECT * FROM desabafo ORDER BY created_at DESC LIMIT ?;", n)
-=======
-	rows, err := db.db.Query("SELECT id, texto, created_at FROM desabafo ORDER BY created_at DESC LIMIT ?", n)
->>>>>>> 4551b46 (Server stub)
+	rows, err := db.db.Query("SELECT id, texto, created_at FROM desabafo ORDER BY created_at DESC LIMIT ?;", n)
 	if err != nil {
 		return nil, err
 	}
